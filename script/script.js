126f5896732deb680699a577afeee37a906104fc
shirtol <shirzahavi7@gmail.com>
2022-05-02 16:04:20 +0300

    Added one change in createGridElements func (instead of adding item to card-front I added it to cardWrap as a data attribute)

--- script/script.js
+++ script/script.js
@@ -68,110 +68,101 @@ function updateCounters({ sidebar, cards }) {
     }
 }
 
+const animals = [
+    "dog",
+    "cat",
+    "duck",
+    "horse",
+    "cat",
+    "dog",
+    "horse",
+    "duck",
+    "mouse",
+    "mouse",
+    "donkey",
+    "donkey",
+];
 
+function createGameBoard(array, rows, cols) {
+    //checked
+    if (array.length !== rows * cols)
+        return "Error occured. Please check array's size";
+    else {
+        const gameBoard = Array.from(Array(rows), () => new Array());
+        // console.log(gameBoard);
+        let c = 0;
+        let newAnimals = new Array();
+        for (let i = 0; i < gameBoard.length; i++) {
+            newAnimals = animals.slice(c, (c = c + cols));
+            console.log(newAnimals);
 
-const animals= ["dog","cat", "duck", "horse", "cat","dog","horse","duck", "mouse", "mouse", "donkey","donkey"];
-
-
-
-
-
-
-function createGameBoard(array, rows, cols) {   //checked
-  if (array.length!== rows*cols) 
-  return "Error occured. Please check array's size";
-else{    
-const gameBoard= Array.from(Array(rows), () => new Array());
-// console.log(gameBoard);
-let c=0;
-let newAnimals= new Array();
-   for (let i = 0; i < gameBoard.length; i++){
-
-      newAnimals= animals.slice(c, c=c+cols); 
-      console.log(newAnimals);
-
-      for(let b = 0, j = 0; b <newAnimals.length, j < cols; b++, j++){
-          
-          gameBoard[i].push(newAnimals[b]);
-          
-      }   
-}
-console.log(gameBoard);
-}
-
+            for (let b = 0, j = 0; b < newAnimals.length, j < cols; b++, j++) {
+                gameBoard[i].push(newAnimals[b]);
+            }
+        }
+        console.log(gameBoard);
+    }
 }
 console.log(createGameBoard(animals, 3, 4));
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-function gameBoardloop(gameBoard){            ////checked
-  for(let i = 0; i < gameBoard.length; i++){
-      for(let j = 0; j < gameBoard[i].length; j++ ){
-          createGridElements(gameBoard[i][j]);
-          // console.log(gameBoard[i][j]);
-      }
-  }
-  
-  }
-  // console.log(gameBoardloop(board));
+function gameBoardloop(gameBoard) {
+    ////checked
+    for (let i = 0; i < gameBoard.length; i++) {
+        for (let j = 0; j < gameBoard[i].length; j++) {
+            createGridElements(gameBoard[i][j]);
+            // console.log(gameBoard[i][j]);
+        }
+    }
+}
+// console.log(gameBoardloop(board));
 //////////////////////////////////////////////////////////////////////////
 
-const grid= document.querySelector(".cards-container");
+const grid = document.querySelector(".cards-container");
 
-function createGridElements(item)  {            ////checked
-const cardScene= document.createElement("div");
-const cardwrap= document.createElement("div");
-const front= document.createElement("div");
-const back= document.createElement("div");
-cardScene.classList.add("card-scene");
-cardwrap.classList.add("card");
-front.classList.add(item);
-front.classList.add("card-front");
-back.classList.add("card-back");
-cardwrap.appendChild(front);
-cardwrap.appendChild(back);
-cardScene.appendChild(cardwrap);
-grid.appendChild(cardScene);
+function createGridElements(item) {
+    ////checked
+    const cardScene = document.createElement("div");
+    const cardWrap = document.createElement("div");
+    const front = document.createElement("div");
+    const back = document.createElement("div");
+    cardScene.classList.add("card-scene");
+    cardWrap.classList.add("card");
+    cardWrap.classList.setAttribute("data-type", item);
+    front.classList.add("card-front");
+    back.classList.add("card-back");
+    cardWrap.appendChild(front);
+    cardWrap.appendChild(back);
+    cardScene.appendChild(cardWrap);
+    grid.appendChild(cardScene);
 }
 
 // console.log(createGridElements("cat"));
 ////////////////////////////////////////////////////////////////////////////////////////
 
-function shuffle(array){     ////checked
+function shuffle(array) {
+    ////checked
     let random;
-    const newShuffledArray= new Array(); 
+    const newShuffledArray = new Array();
     console.log(newShuffledArray);
 
-    for(let i = 0; i< array.length; i++){
-        random= getRandomIntInclusive(0,array.length-1);
-        while (newShuffledArray[random]!==undefined){
-        random= getRandomIntInclusive(0,array.length-1);
-    }
-        newShuffledArray[random] = array[i];    
+    for (let i = 0; i < array.length; i++) {
+        random = getRandomIntInclusive(0, array.length - 1);
+        while (newShuffledArray[random] !== undefined) {
+            random = getRandomIntInclusive(0, array.length - 1);
+        }
+        newShuffledArray[random] = array[i];
     }
     console.log(newShuffledArray);
 }
 // ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
-function getRandomIntInclusive(min, max) {     ////checked
+function getRandomIntInclusive(min, max) {
+    ////checked
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min)
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 // console.log(getRandomIntInclusive(0,12));
 
-console.log((shuffle(animals)));
-
-
-
-
-
-
-
-
-
-
-
-
-            
-
+console.log(shuffle(animals));
