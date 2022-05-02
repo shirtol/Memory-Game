4728a84de05bf7069c56b243883077fa7ff145f8
KeterPerry <keterav@gmail.com>
2022-05-02 13:37:43 +0300

    hope now its o.k

--- script/script.js
+++ script/script.js
@@ -1,53 +1,53 @@
 
-// import { addFlipCardEvent, observeNumOfFlippedCards } from "./Cards.js";
-// import { GameState } from "./GameState.js";
+import { addFlipCardEvent, observeNumOfFlippedCards } from "./Cards.js";
+import { GameState } from "./GameState.js";
 
-// const gameState = new GameState();
+const gameState = new GameState();
 
-// observeNumOfFlippedCards(gameState);
-// addFlipCardEvent(gameState);
+observeNumOfFlippedCards(gameState);
+addFlipCardEvent(gameState);
 
-// function timer() {
-//   const timerCount = document.querySelector(".timer .count");
-//   let seconds = 0,
-//     mins = 0;
-//   setInterval(() => {
-//     let total = mins < 10 ? "0" + mins : mins;
-//     seconds++;
-//     timerCount.innerText =
-//       seconds < 10 ? total + ":0" + seconds : total + ":" + seconds;
-//     if (seconds === 59) {
-//       seconds = -1;
-//       mins++;
-//     }
-//     //! settimeout limit if needed to end game here.
-//     // if(mins === 60){
+function timer() {
+  const timerCount = document.querySelector(".timer .count");
+  let seconds = 0,
+    mins = 0;
+  setInterval(() => {
+    let total = mins < 10 ? "0" + mins : mins;
+    seconds++;
+    timerCount.innerText =
+      seconds < 10 ? total + ":0" + seconds : total + ":" + seconds;
+    if (seconds === 59) {
+      seconds = -1;
+      mins++;
+    }
+    //! settimeout limit if needed to end game here.
+    // if(mins === 60){
 
-//     // }
-//   }, 1000);
-// }
-// timer();
+    // }
+  }, 1000);
+}
+timer();
 
-// //! need to pass cards number of pairs and the counter of successes.
-// function gameOver(successCounter, cardsNum) {
-//   if (successCounter === cardsNum) {
-//     return true;
-//   }
-//   return false;
-// }
+//! need to pass cards number of pairs and the counter of successes.
+function gameOver(successCounter, cardsNum) {
+  if (successCounter === cardsNum) {
+    return true;
+  }
+  return false;
+}
 
-// //! pass the guessesCount array from the game main obj
-// function updateCounters({ guessesCount }) {
-//   //! put flipCard func insted of true to see if second card flip is success or fail
-//   if (true) {
-//     document.querySelector(".correct-count").innerText = ++guessesCount[0];
-//   } else {
-//     document.querySelector(".incorrect-count").innerText = ++guessesCount[1];
-//   }
-// }
+//! pass the guessesCount array from the game main obj
+function updateCounters({ guessesCount }) {
+  //! put flipCard func insted of true to see if second card flip is success or fail
+  if (true) {
+    document.querySelector(".correct-count").innerText = ++guessesCount[0];
+  } else {
+    document.querySelector(".incorrect-count").innerText = ++guessesCount[1];
+  }
+}
 
-// //! exmaple of main game obj, add here matrix, array of divs and other properties
-// const obj = { guessesCount: [0, 0] };
+//! exmaple of main game obj, add here matrix, array of divs and other properties
+const obj = { guessesCount: [0, 0] };
 
 
 const animals= ["dog","cat", "duck", "horse", "cat","dog","horse","duck", "mouse", "mouse", "donkey","donkey"];
